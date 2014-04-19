05fa3243bb90042505cdb568da41c354aedb35f6
keithGrout <keiwi.g@gmail.com>
2014-04-19 13:44:57 -0700

    comment flyover of stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -17,3 +17,12 @@ var makeStack = function(){
 
   return instance;
 };
+
+// a stack works like a set of plates
+// we must add to the top and take from the top (last in, first out LIFO)
+
+// we can use a storage object (within an instance of an object) with numeric keys to store each new item.
+// by keeping a ref to the size, the last thing we put in will be equal to the current size - 1 (indexing by zero)
+// to add something we can store it on our stack object inside a storage object with a numeric key equal to size
+// to remove something we can delete the property referred to by size - 1.
+// we will add all objects or values and remove all objects/valuesfrom a general storage object.
