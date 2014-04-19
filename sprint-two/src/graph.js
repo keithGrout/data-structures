28b1bfc3d03f1a7c69336c66d5bc5b63ac4630a1
Hack Reactor Students <keiwi.g@gmail.com>
2014-04-19 16:16:56 -0700

    updating spec

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,13 +1,54 @@
 var Graph = function(){
+
+  // this.puppies = {
+  //   next: kittens;
+  // }
+  // this.kittens = {
+  //   next : puppies
+  // }
+
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+    var key;
+    this[newNode] = {};
+
+
+    if(Object.keys(this).length === 2){
+      for(key in this){
+        if(this[key] === {} && Object.keys(this[key]).length === 0){
+          this[key].next = newNode;
+          this[newNode].next = this[key];
+        }
+      }
+    }
+
+
+    if (toNode) {
+      this[toNode] = {};
+      this[toNode].next = newNode;
+      this[newNode].next = toNode;
+    }
+
 };
 
 Graph.prototype.contains = function(node){
+  var key;
+  for(key in this){
+    if(key === node){
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  var key;
+  for (key in this) {
+    if (key === node) {
+      delete this[key];
+    }
+  }
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
@@ -18,3 +59,18 @@ Graph.prototype.addEdge = function(fromNode, toNode){
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
 };
+
+var graph = new Graph();
+graph.addNode("puppies");
+graph.addNode("kittens");
+// addNode(kittens, puppies);
+// addNode(sharks, kittens);
+
+// {
+//   kittens = {
+//     next : puppies
+//   }
+//   sharks = {
+//     next: kittens
+//   }
+// }
